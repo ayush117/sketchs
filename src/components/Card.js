d682e8d5b2c4246c5d1389fb16fe72284c3e0360
Divyanshu Tare <divyanshutare18@gmail.com>
2023-01-05 12:08:11 +0530

    home page added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,28 @@
 const Card = () => {
-  return (
-      <div className="p-4 pb-3.5 px-4 rounded-none shadow-lg bg-white max-w-xs">
-        <div className="justify-center">
-          <img
-            className="rounded-r-xs"
-            src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
-            alt=""
-          />
-        </div>
-        <div className="d-flex justify-content-between">
-          <h2 className="text-lg mb-2">Title
-            <span className="text-sm text-teal-800 font-mono bg-teal-100 inline rounded-full px-2 align-top float-right animate-pulse">
-              Tag
-            </span>
-          </h2>
-          Author    
-        </div>
+  return <>
+      <div className="card flex flex-col border-2 border-black p-4" style={{flexBasis: "32%"}}>
+          <div className="image flex justify-center pb-2">
+              <img src="/images/dummy.png" alt="dummy"></img>
+          </div>
+          <div className="card-info flex flex-col">
+              <div className="flex justify-between">
+                  <div>
+                      Title
+                  </div>
+                  <div className="inline-flex">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                      </svg>
+                      <span className="text-lg relative bottom-1">5</span>
+                  </div>
+              </div>
+              <div className="text-left">
+                  Author
+              </div>
+          </div>
+
       </div>
-  );
-};
+  </>
+}
 
 export default Card;
